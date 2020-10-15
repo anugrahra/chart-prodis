5f6669581d738af577616a458eff079a2eedbb28
Anugrah Ramadhan <dialog.anugrah@gmail.com>
2020-10-15 23:14:36 +0700

    add another charts and splitting js and css into different files

--- index.php
+++ index.php
@@ -2,100 +2,41 @@
 <html>
 
 <head>
-    <title>Grafik Prodis BLUD Air Minum Kota Cimahi</title>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            font-family: Arial, Helvetica, sans-serif;
-        }
-
-        .canvas-container {
-            width: 83%;
-        }
-    </style>
+  <title>Grafik Prodis BLUD Air Minum Kota Cimahi</title>
+  <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
+  <header>
     <h3>GRAFIK PRODIS BLUD AIR MINUM KOTA CIMAHI</h3>
+    <hr>
+  </header>
+  <section>
+    <h4>Grafik Produksi dan Distribusi Air Minum</h4>
     <div class="canvas-container">
-        <canvas id="graphCanvas"></canvas>
+      <canvas id="graphCanvas"></canvas>
     </div>
+  </section>
+  <section>
+    <h4>Grafik Pemakaian Bahan Kimia</h4>
+    <div class="canvas-container">
+      <canvas id="graphCanvasKimia"></canvas>
+    </div>
+  </section>
+  <section>
+    <h4>Grafik Dosis Koagulan & Disinfektan</h4>
+    <div class="canvas-container">
+      <canvas id="graphCanvasDosis"></canvas>
+    </div>
+  </section>
 
-    <script>
-        $(document).ready(function() {
-            showGraph();
-        });
+  <footer>
+    <p>&copy; 2020. dibuat dengan ❤️ oleh <a href="https://anugrah.club">anugrah</a> untuk BLUD AM Cimahi</p>
+  </footer>
 
-
-        function showGraph() {
-            {
-                $.post("data.php",
-                    function(data) {
-                        console.log(data);
-                        let bulan = [];
-                        let produksi = [];
-                        let distribusi = [];
-                        let pemair = [];
-                        let nrw = [];
-
-                        for (let i in data) {
-                            bulan.push(data[i].bulan);
-                            produksi.push(data[i].jumlah_produksi);
-                            distribusi.push(data[i].jumlah_distribusi);
-                            pemair.push(data[i].pemair);
-                            nrw.push(data[i].nrw);
-                        }
-
-                        let chartdata = {
-                            labels: bulan,
-                            datasets: [{
-                                label: 'Produksi',
-                                backgroundColor: '#FF7F00',
-                                borderColor: '#FF7F00',
-                                hoverBackgroundColor: '#CCCCCC',
-                                hoverBorderColor: '#666666',
-                                fill: false,
-                                data: produksi
-                            }, {
-                                label: 'Distribusi',
-                                backgroundColor: '#0000FF',
-                                borderColor: '#0000FF',
-                                hoverBackgroundColor: '#CCCCCC',
-                                hoverBorderColor: '#666666',
-                                fill: false,
-                                data: distribusi
-                            }, {
-                                label: 'Pemair',
-                                backgroundColor: '#00FF00',
-                                borderColor: '#00FF00',
-                                hoverBackgroundColor: '#CCCCCC',
-                                hoverBorderColor: '#666666',
-                                fill: false,
-                                data: pemair
-                            }, {
-                                label: 'NRW',
-                                backgroundColor: '#000000',
-                                borderColor: '#000000',
-                                hoverBackgroundColor: '#CCCCCC',
-                                hoverBorderColor: '#666666',
-                                fill: false,
-                                data: nrw
-                            }]
-                        };
-
-                        let graphTarget = $("#graphCanvas");
-
-                        let barGraph = new Chart(graphTarget, {
-                            type: 'line',
-                            data: chartdata
-                        });
-                    });
-            }
-        }
-    </script>
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
+  <script src="js/script.js"></script>
 
 </body>
 
---
+++ js/script.js
@@ -0,0 +1,131 @@
+$(document).ready(function() {
+  showGraph();
+});
+
+function showGraph() {
+  {
+    $.post("data.php",
+      function(data) {
+        console.log(data);
+        let bulan = [];
+        let produksi = [];
+        let distribusi = [];
+        let pemair = [];
+        let nrw = [];
+        let pac = [];
+        let kaporit = [];
+        let dosisPac = [];
+        let dosisKaporit = [];
+
+        for (let i in data) {
+          bulan.push(data[i].bulan);
+          produksi.push(data[i].jumlah_produksi);
+          distribusi.push(data[i].jumlah_distribusi);
+          pemair.push(data[i].pemair);
+          nrw.push(data[i].nrw);
+          pac.push(data[i].pac);
+          kaporit.push(data[i].kaporit);
+          dosisPac.push(data[i].dosis_pac);
+          dosisKaporit.push(data[i].dosis_kaporit);
+        }
+
+        let chartdata = {
+          labels: bulan,
+          datasets: [{
+            label: 'Produksi (m3)',
+            backgroundColor: '#FF7F00',
+            borderColor: '#FF7F00',
+            hoverBackgroundColor: '#CCCCCC',
+            hoverBorderColor: '#666666',
+            fill: false,
+            data: produksi
+          }, {
+            label: 'Distribusi (m3)',
+            backgroundColor: '#0000FF',
+            borderColor: '#0000FF',
+            hoverBackgroundColor: '#CCCCCC',
+            hoverBorderColor: '#666666',
+            fill: false,
+            data: distribusi
+          }, {
+            label: 'Pemair (m3)',
+            backgroundColor: '#00FF00',
+            borderColor: '#00FF00',
+            hoverBackgroundColor: '#CCCCCC',
+            hoverBorderColor: '#666666',
+            fill: false,
+            data: pemair
+          }, {
+            label: 'NRW (m3)',
+            backgroundColor: '#000000',
+            borderColor: '#000000',
+            hoverBackgroundColor: '#CCCCCC',
+            hoverBorderColor: '#666666',
+            fill: false,
+            data: nrw
+          }]
+        };
+
+        let chartdataKimia = {
+          labels: bulan,
+          datasets: [{
+            label: 'PAC (Kg)',
+            backgroundColor: '#FF7F00',
+            borderColor: '#FF7F00',
+            hoverBackgroundColor: '#CCCCCC',
+            hoverBorderColor: '#666666',
+            fill: false,
+            data: pac
+          }, {
+            label: 'Kaporit (Kg)',
+            backgroundColor: '#0000FF',
+            borderColor: '#0000FF',
+            hoverBackgroundColor: '#CCCCCC',
+            hoverBorderColor: '#666666',
+            fill: false,
+            data: kaporit
+          }]
+        };
+
+        let chartdataDosis = {
+          labels: bulan,
+          datasets: [{
+            label: 'Koagulan / PAC (mg/l)',
+            backgroundColor: '#FF7F00',
+            borderColor: '#FF7F00',
+            hoverBackgroundColor: '#CCCCCC',
+            hoverBorderColor: '#666666',
+            fill: false,
+            data: dosisPac
+          }, {
+            label: 'Disinfektan / Kaporit (mg/l)',
+            backgroundColor: '#0000FF',
+            borderColor: '#0000FF',
+            hoverBackgroundColor: '#CCCCCC',
+            hoverBorderColor: '#666666',
+            fill: false,
+            data: dosisKaporit
+          }]
+        };
+
+        let graphTarget = $("#graphCanvas");
+        let lineGraph = new Chart(graphTarget, {
+          type: 'line',
+          data: chartdata,
+        });
+
+        let graphTargetKimia = $("#graphCanvasKimia");
+        let lineGraphKimia = new Chart(graphTargetKimia, {
+          type: 'line',
+          data: chartdataKimia
+        });
+
+        let graphTargetDosis = $("#graphCanvasDosis");
+        let lineGraphDosis = new Chart(graphTargetDosis, {
+          type: 'line',
+          data: chartdataDosis
+        });
+
+      });
+  }
+}
